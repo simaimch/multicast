a6f6b8d7aeb71eb328c0a94a1b8413051e12204b
Christian Hoffmann <christian.fr.hoffmann@gmail.com>
2024-11-30 15:47:24 +0100

    add `Webpack`

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+    target: 'node',
+    entry: './bin/server.js',
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    // Additional configuration goes here
+};
