f25bce7499da97ec3de0bfefd9782231392642dd
Christian Hoffmann <christian.fr.hoffmann@gmail.com>
2024-12-01 06:19:06 +0100

    add listen to `index.html`

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import path from "path";
 
 //const express = require('express')
 const app = express()
@@ -13,6 +14,18 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json(), cors());
 
 app.disable('x-powered-by');
+
+// Serve Up index.html
+app.use((req, res, next) => {
+    if (req.method != 'GET' || req.path.includes('/socket.io/') || /\.(ico|js|css|jpg|png|map|ttf)$/i.test(req.path)) {
+        next();
+    } else {
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        res.header('Expires', '-1');
+        res.header('Pragma', 'no-cache');
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    }
+});
 
 app.use('*', async (req: Request, res: Response, next) => {
     res.status(404).end(`"404: Service not provided: ${req.originalUrl}"`);
