ab57dcfa743be5bbd2e37920edeb9f6bd4d801d9
Christian Hoffmann <christian.fr.hoffmann@gmail.com>
2024-12-01 08:23:15 +0100

    fix webpack of server, because we use several packages in the bin-subfolder now

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     target: 'node',
-    entry: './bin/server.js',
+    entry: './bin/server/src/server.js',
     output: {
         filename: 'server.js',
         path: path.resolve(__dirname, 'dist'),
