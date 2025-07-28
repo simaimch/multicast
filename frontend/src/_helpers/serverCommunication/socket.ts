5bbe88268159421fbc20f2d97b3329e8fe663817
Christian Hoffmann <christian.fr.hoffmann@gmail.com>
2025-07-28 10:54:55 +0200

    change domain of `frontend`-socket-connection

--- frontend/src/_helpers/serverCommunication/socket.ts
+++ frontend/src/_helpers/serverCommunication/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? 'https://85.215.242.26:8719/' : 'https://85.215.242.26:8719/';
+const URL = process.env.NODE_ENV === 'production' ? 'https://edufant.eu:8719/' : 'https://edufant.eu:8719/';
 
 export const socket = io(URL, {autoConnect: false});
